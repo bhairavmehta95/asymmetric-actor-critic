70160ebfb096ab1a8e6412c317fbc0a4f3a31596
Bhairav Mehta <bhairavm@umich.edu>
2017-12-03 02:15:41 -0500

    adding goal generation and image observationS

--- environment/particle.py
+++ environment/particle.py
@@ -12,6 +12,7 @@ import numpy as np
 from gym import spaces
 from gym import utils
 from gym.envs.mujoco import mujoco_env
+from scipy import misc
 
 t = 0
 
@@ -19,49 +20,65 @@ class ParticleEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         mujoco_env.MujocoEnv.__init__(self, 'particle.xml', 5)
         utils.EzPickle.__init__(self)
-        self._get_viewer()
+        self.state = None
 
 
     def _step(self, a):
         xpos = self.get_body_com("agent")[0]
         self.do_simulation(a, self.frame_skip)
+        self.state = state = self.state_vector()
 
-        state = self.state_vector()
         notdone = np.isfinite(state).all() \
             and state[2] >= 0.2 and state[2] <= 1.0
         reward = 0
 
         done = not notdone
-        ob = self._get_obs()
 
         image_obs = np.zeros([500, 500, 3])
 
         if self.viewer is not None:
-            data = self.viewer.get_image()
-            
-            img_data = data[0]
-            width = data[1]
-            height = data[2]
-            tmp = np.fromstring(img_data, dtype=np.uint8)
-            image_obs = np.reshape(tmp, [height, width, 3])
-            image_obs = np.flipud(image_obs)
-
+            image_obs = self._get_image()
 
         return image_obs, state, reward, done
 
 
-    def _get_obs(self):
-        return np.concatenate([
-            self.model.data.qpos.flat[2:],
-            self.model.data.qvel.flat,
-        ])
+    def _get_image(self):
+        self.render()
+        data = self.viewer.get_image()
+        
+        img_data = data[0]
+        width = data[1]
+        height = data[2]
+        tmp = np.fromstring(img_data, dtype=np.uint8)
+        image_obs = np.reshape(tmp, [height, width, 3])
+        image_obs = np.flipud(image_obs)
+
+        return image_obs
+
+
+
+    def _at_goal(self):
+        pass
+
+
+    def _escaped(self):
+        pass
+
+
+    def _get_goal(self, goal):
+        qpos = goal['qpos']
+        qvel = goal['qvel']
+
+        self.set_state(qpos, qvel)
+        goal_obs = self._get_image()
+        return goal_obs
 
 
     def reset_model(self):
         qpos = self.init_qpos + self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
         qvel = self.init_qvel + self.np_random.randn(self.model.nv) * .1
         self.set_state(qpos, qvel)
-        return self._get_obs()
+        return self._get_image()
 
 
     def viewer_setup(self):
@@ -74,6 +91,16 @@ def run():
     env = ParticleEnv()
     env.reset()
 
+    goalpos = np.random.uniform(size=env.model.nq, low=1.1, high=1.4)
+    goalvel = np.zeros(shape=env.model.nq)
+
+    goal = dict()
+    goal['qpos'] = goalpos
+    goal['qvel'] = goalvel
+    goal_obs = env._get_goal(goal)
+
+    env.reset()
+
     t = 0
 
     action = None
@@ -82,9 +109,10 @@ def run():
 
         # print(width, height)
         # print(obs)
+
         obs, state, r, done = env.step(np.random.uniform(low=-0.1, high=0.1))
         if done:
-            print('boop!')
+            # print('boop!')
             env.reset()
         t += 1
 
